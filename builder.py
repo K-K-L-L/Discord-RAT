2b04406236782598ccf2b3b7d6bee75c9d0b8656
K-K-L-L <124736630+K-K-L-L@users.noreply.github.com>
2024-01-27 19:11:04 +0200

    Create builder.py

---
+++ builder.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+from tkinter import messagebox
+import os, shutil
+
+def submit_forms():
+    global guild_id
+    global bot_token
+    guild_id = guild_id_entry.get()
+    bot_token = bot_token_entry.get()
+    
+    print("Guild ID:", guild_id)
+    print("Bot Token:", bot_token)
+    
+    messagebox.showinfo("Success", "Forms submitted successfully!")
+
+    root.destroy()
+
+root = tk.Tk()
+root.title("App")
+root.geometry("300x200")
+root.resizable(False, False)
+
+guild_id_label = tk.Label(root, text="Guild ID:")
+guild_id_label.pack()
+
+guild_id_entry = tk.Entry(root)
+guild_id_entry.pack()
+
+bot_token_label = tk.Label(root, text="Bot Token:")
+bot_token_label.pack()
+
+bot_token_entry = tk.Entry(root)
+bot_token_entry.pack()
+
+submit_button = tk.Button(root, text="Submit", command=submit_forms)
+submit_button.pack()
+
+root.mainloop()
+
+
+with open("temp.txt", "r") as file:
+    file_contents = file.read()
+
+file_contents = file_contents.replace("{token}", f"\"{bot_token}\"").replace("{guildid}", guild_id)
+
+with open("temp2.py", "w") as file:
+    file.write(file_contents)
+
+os.system("pyinstaller --onefile temp2.py")
+
+os.remove("temp2.py")
+shutil.rmtree("build")
+os.remove("temp2.spec")
